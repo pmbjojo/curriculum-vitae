624d9a1f515c6f62743560b53273ccf3a95f6b9a
pmbjojo <jourdamj@gmail.com>
2025-03-31 17:35:59 +0200

    fix: cv spacing on mobile

--- src/app/pdf/_components/a4.tsx
+++ src/app/pdf/_components/a4.tsx
@@ -3,7 +3,7 @@ import { ComponentProps } from "react";
 export function A4(props: ComponentProps<"div">) {
   return (
     <div
-      className="font-cv w-[210mm] h-[297mm] box-border mx-auto bg-white overflow-hidden flex flex-col text-black print:m-0 print:w-screen print:rounded-none print:shadow-none rounded-md shadow-lg m-10 overflow-x-auto"
+      className="font-cv w-[210mm] h-[297mm] box-border mx-auto bg-white overflow-hidden flex flex-col text-black print:mx-0 print:my-0 md:my-10 print:w-screen print:rounded-none print:shadow-none md:rounded-md shadow-lg"
       {...props}
     >
       {props.children}
