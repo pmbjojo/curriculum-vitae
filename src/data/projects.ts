f2e42392ebb3bf5867c1d9debc14b37708481e36
pmbjojo <jourdamj@gmail.com>
2025-04-10 17:17:21 +0200

    docs: add junit to be an artist

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -65,7 +65,7 @@ export const projects: TProject[] = [
     summary: "Application de bureau de dessin avec divers outils",
     description:
       "Une application Java permettant de dessiner avec divers outils (pinceaux, formes, couleurs). Développée dans le cadre d'un projet scolaire pour apprendre à utiliser Java Swing et la programmation orientée objet.",
-    technologies: ["Java", "Swing"],
+    technologies: ["Java", "Swing", "JUnit"],
     type: "Scolaire",
     year: 2022,
     repository: "https://github.com/pmbjojo/be-an-artist",
