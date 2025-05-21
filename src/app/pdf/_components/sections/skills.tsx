7470b10e20918780fceb5a473603a78b6f16cb0c
pmbjojo <jourdamj@gmail.com>
2025-05-21 23:16:48 +0200

    fix: re add soft skills to pdf page

--- src/app/pdf/_components/sections/skills.tsx
+++ src/app/pdf/_components/sections/skills.tsx
@@ -1,11 +1,17 @@
 import { resume } from "@/data";
 import { Section } from "../section";
+import { TSkill } from "@/schemas/skill";
+
+const skills: TSkill[] = [
+  ...resume.skills,
+  { domain: "Comportementales", items: resume.softSkills.map((s) => s.name) },
+];
 
 export function Skills() {
   return (
     <Section title="Compétences">
       <ul className="pr-5">
-        {resume.skills.map((s) => (
+        {skills.map((s) => (
           <li key={s.domain}>
             <h3 className="font-bold">{s.domain}</h3>
             <div>{s.items.join(", ")}</div>
