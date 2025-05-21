4eb6136534af6a7bb0e83d6ef2a155a57b9f8936
pmbjojo <jourdamj@gmail.com>
2025-05-21 12:12:26 +0200

    fix: particles rerender on navigation

--- src/components/web/particles-provider.tsx
+++ src/components/web/particles-provider.tsx
@@ -1,6 +1,10 @@
 "use client";
 
-import { useEffect, useMemo, useState } from "react";
+import {
+  useEffect,
+  // useMemo,
+  // useState
+} from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import {
   type ISourceOptions,
@@ -8,58 +12,97 @@ import {
   OutMode,
 } from "@tsparticles/engine";
 import { loadSlim } from "@tsparticles/slim";
-import { useTheme } from "next-themes";
+// import { useTheme } from "next-themes";
+
+const options: ISourceOptions = {
+  fpsLimit: 120,
+  particles: {
+    color: {
+      value: "#ffe0c2",
+      // value: resolvedTheme === "dark" ? "#ffe0c2" : "#644a40",
+    },
+    move: {
+      direction: MoveDirection.none,
+      enable: true,
+      outModes: {
+        default: OutMode.out,
+      },
+      random: false,
+      speed: 0.1,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+      },
+      value: 60,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      value: { min: 1, max: 3 },
+    },
+  },
+  detectRetina: true,
+};
 
 export function ParticlesProvider() {
-  const [init, setInit] = useState(false);
-  const { resolvedTheme } = useTheme();
+  console.log("ParticlesProvider");
+  // const [init, setInit] = useState(false);
+  // const { resolvedTheme } = useTheme();
   useEffect(() => {
     initParticlesEngine(async (engine) => {
       await loadSlim(engine);
     }).then(() => {
-      setInit(true);
+      // setInit(true);
     });
   }, []);
 
-  const options: ISourceOptions = useMemo(
-    () => ({
-      fpsLimit: 120,
-      particles: {
-        color: {
-          value: resolvedTheme === "dark" ? "#ffe0c2" : "#644a40",
-        },
-        move: {
-          direction: MoveDirection.none,
-          enable: true,
-          outModes: {
-            default: OutMode.out,
-          },
-          random: false,
-          speed: 0.1,
-          straight: false,
-        },
-        number: {
-          density: {
-            enable: true,
-          },
-          value: 60,
-        },
-        opacity: {
-          value: 0.5,
-        },
-        shape: {
-          type: "circle",
-        },
-        size: {
-          value: { min: 1, max: 3 },
-        },
-      },
-      detectRetina: true,
-    }),
-    [resolvedTheme]
-  );
+  // const options: ISourceOptions = useMemo(
+  //   () => ({
+  //     fpsLimit: 120,
+  //     particles: {
+  //       color: {
+  //         value: resolvedTheme === "dark" ? "#ffe0c2" : "#644a40",
+  //       },
+  //       move: {
+  //         direction: MoveDirection.none,
+  //         enable: true,
+  //         outModes: {
+  //           default: OutMode.out,
+  //         },
+  //         random: false,
+  //         speed: 0.1,
+  //         straight: false,
+  //       },
+  //       number: {
+  //         density: {
+  //           enable: true,
+  //         },
+  //         value: 60,
+  //       },
+  //       opacity: {
+  //         value: 0.5,
+  //       },
+  //       shape: {
+  //         type: "circle",
+  //       },
+  //       size: {
+  //         value: { min: 1, max: 3 },
+  //       },
+  //     },
+  //     detectRetina: true,
+  //   }),
+  //   [
+  //     resolvedTheme
+  //   ]
+  // );
 
-  if (init) {
-    return <Particles className="-z-10" id="tsparticles" options={options} />;
-  }
+  // if (init) {
+  return <Particles className="-z-10" id="tsparticles" options={options} />;
+  // }
 }
--- src/components/web/tab-button.tsx
+++ src/components/web/tab-button.tsx
@@ -1,7 +1,7 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { ComponentProps } from "react";
+import { ComponentProps, useMemo } from "react";
 
 export function TabButton({
   children,
@@ -12,14 +12,18 @@ export function TabButton({
   href: string;
 }) {
   const path = usePathname();
-  const isActive = path === href;
+  const activeClass = useMemo(
+    () =>
+      path === href ? "bg-background shadow-sm text-foreground" : undefined,
+    [path, href]
+  );
   return (
     <Link
       role="tab"
       href={href}
       className={cn(
         "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:outline-ring inline-flex flex-1 items-center justify-center gap-1.5 rounded-md px-2 py-3 text-sm font-medium whitespace-nowrap transition-[color,box-shadow] focus-visible:ring-[3px] focus-visible:outline-1 disabled:pointer-events-none disabled:opacity-50",
-        isActive ? "bg-background shadow-sm text-foreground" : undefined,
+        activeClass,
         className
       )}
       {...props}
