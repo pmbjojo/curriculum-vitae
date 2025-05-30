e614029d3a2ec3a5dfdfa15099272174c3c48c8f
pmbjojo <jourdamj@gmail.com>
2025-05-31 00:28:44 +0200

    fix: add padding to skills section

--- src/app/(web)/_components/skills-section.tsx
+++ src/app/(web)/_components/skills-section.tsx
@@ -32,7 +32,7 @@ export const skills: Skill[] = [
 
 export default function SkillsSection() {
   return (
-    <section className="flex flex-col items-center gap-10">
+    <section className="flex flex-col items-center gap-10 px-4">
       <Carousel
         className="container px-4"
         opts={{
@@ -48,21 +48,21 @@ export default function SkillsSection() {
           {skills.map((skill) => (
             <CarouselItem
               key={skill}
-              className=" sm:basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
+              className="sm:basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
             >
               <Link
                 href={SkillMeta[skill].url ?? ""}
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <Card className="shadow-none hover:shadow-sm select-none">
+                <Card className="shadow-none select-none hover:shadow-sm">
                   <CardContent className="flex flex-col items-center justify-center gap-3">
                     <Image
                       src={SkillMeta[skill].icon}
                       alt={skill}
                       className="size-16"
                     />
-                    <h4 className="font-medium text-center">{skill}</h4>
+                    <h4 className="text-center font-medium">{skill}</h4>
                   </CardContent>
                 </Card>
               </Link>
@@ -70,13 +70,13 @@ export default function SkillsSection() {
           ))}
         </CarouselContent>
       </Carousel>
-      <div className="grid grid-cols-2 lg:grid-cols-4 gap-5 h-full">
+      <div className="grid h-full grid-cols-2 gap-5 lg:grid-cols-4">
         {resume.softSkills.map((s) => {
           const Icon = SoftSkillMeta[s.name].icon;
           return (
             <Popover key={s.name}>
               <PopoverTrigger>
-                <Card className="flex-row p-3 cursor-pointer">
+                <Card className="cursor-pointer flex-row p-3">
                   <Icon />
                   <span>{s.name}</span>
                 </Card>
