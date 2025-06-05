54cfcb767fcfaaf10b8e300c1e5f271690728bf9
pmbjojo <jourdamj@gmail.com>
2025-06-06 00:19:15 +0200

    fix: sort projects by data

--- src/app/(web)/projects/page.tsx
+++ src/app/(web)/projects/page.tsx
@@ -20,29 +20,31 @@ import { resume } from "@/data";
 export default function ProjectsTab() {
   return (
     <Timeline>
-      {resume.projects.map((p, i) => (
-        <TimelineCard index={i} key={p.title}>
-          <Header>
-            <HeaderTop>
-              <Title>{p.title}</Title>
-              <HeaderAction>
-                {p.website && <WebSite href={p.website} />}
-                {p.repository && <Repository href={p.repository} />}
-              </HeaderAction>
-            </HeaderTop>
-            <HeaderDescription>
-              <Institution institution={{ name: p.type }} />
-              <Period>{p.year.toString()}</Period>
-            </HeaderDescription>
-          </Header>
-          <Content>
-            <div className="flex flex-col gap-2">
-              <p>{p.description}</p>
-              <BadgeList items={p.technologies} />
-            </div>
-          </Content>
-        </TimelineCard>
-      ))}
+      {resume.projects
+        .toSorted((a, b) => b.year - a.year)
+        .map((p, i) => (
+          <TimelineCard index={i} key={p.title}>
+            <Header>
+              <HeaderTop>
+                <Title>{p.title}</Title>
+                <HeaderAction>
+                  {p.website && <WebSite href={p.website} />}
+                  {p.repository && <Repository href={p.repository} />}
+                </HeaderAction>
+              </HeaderTop>
+              <HeaderDescription>
+                <Institution institution={{ name: p.type }} />
+                <Period>{p.year.toString()}</Period>
+              </HeaderDescription>
+            </Header>
+            <Content>
+              <div className="flex flex-col gap-2">
+                <p>{p.description}</p>
+                <BadgeList items={p.technologies} />
+              </div>
+            </Content>
+          </TimelineCard>
+        ))}
     </Timeline>
   );
 }
