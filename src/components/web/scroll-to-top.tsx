2c72458b26fc958af0be3a8ec282e9beb43af3ae
pmbjojo <jourdamj@gmail.com>
2025-06-16 17:01:13 +0200

    style: animate scroll to top entrance and exit

--- src/components/web/scroll-to-top.tsx
+++ src/components/web/scroll-to-top.tsx
@@ -3,50 +3,48 @@
 import React, { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { ArrowUpIcon } from "lucide-react";
+import { AnimatePresence, motion } from "motion/react";
 
 const ScrollToTopButton = () => {
   const [showButton, setShowButton] = useState(false);
-
-  // Function to show or hide the button based on scroll position
   const checkScroll = () => {
     if (window.scrollY > 300) {
-      // Customize the scroll position as needed
       setShowButton(true);
     } else {
       setShowButton(false);
     }
   };
 
-  // Scroll event listener setup
   useEffect(() => {
     window.addEventListener("scroll", checkScroll);
-
-    // Clean up the event listener on component unmount
     return () => {
       window.removeEventListener("scroll", checkScroll);
     };
   }, []);
 
-  // Scroll to the top of the page
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: "smooth", // Optional smooth scrolling
+      behavior: "smooth",
     });
   };
 
   return (
-    <>
+    <AnimatePresence>
       {showButton && (
-        <Button
-          onClick={scrollToTop}
-          className="fixed bottom-4 right-4 z-50"
-          size="icon"
+        <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: 100 }}
+          transition={{ duration: 0.5 }}
+          className="fixed right-4 bottom-4 z-50"
         >
-          <ArrowUpIcon />
-        </Button>
+          <Button onClick={scrollToTop} size="icon">
+            <ArrowUpIcon />
+          </Button>
+        </motion.div>
       )}
-    </>
+    </AnimatePresence>
   );
 };
 
